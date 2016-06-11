19ecc2b69af100c5d66a13d7b6f2419a08819557
momolafripouille <Nicolas-g7@hotmail.fr>
2016-06-11 14:01:54 +0200

    single trainings

--- client/templates/training.js
+++ client/templates/training.js
@@ -1,30 +1,14 @@
-/**
- * Created by nicolas on 12/05/2016.
- */
-Template.training-item.onRendered(function() {
-    var self = this;
+Template.training.helpers({
 
-    // If the activity is in a list, scroll it into view. Note, we can't just use
-    // element.scrollIntoView() because it attempts to scroll in the X direction
-    // messing up our animations
-    if (Router.current().params.trainingId === self.data._id) {
-        var $training = $(self.firstNode);
-        var top = $training.offset().top;
-        var $parent = $(self.firstNode).closest('.content-scrollable');
-        var parentTop = $parent.offset().top;
-        $parent.scrollTop(top - parentTop);
+
+    trainings: function() {
+        return Trainings.find({ trainingName: Router.current().params.name });
+    },
+    ready: function() {
+        return Router.current().training.ready();
+
     }
 });
-
-Template.training-item.helpers({
-    firstName: function() {
-        return this.userName.split(' ')[0];
-    },
-    recipeTitle: function() {
-        return RecipesData[this.trainingName].title;
-    },
-    path: function() {
-        return Router.path('recipe', { name: this.trainingName },
-            { query: { trainingId: this._id } })
-    }
-})
+Template.training.onCreated(function() {
+    console.log(Router.current().params.name)
+});
---
+++ client/templates/training_item.js
@@ -0,0 +1,30 @@
+/**
+ * Created by nicolas on 12/05/2016.
+ */
+Template.training_item.onRendered(function() {
+    var self = this;
+
+    // If the activity is in a list, scroll it into view. Note, we can't just use
+    // element.scrollIntoView() because it attempts to scroll in the X direction
+    // messing up our animations
+    if (Router.current().params.trainingId === self.data._id) {
+        var $training = $(self.firstNode);
+        var top = $training.offset().top;
+        var $parent = $(self.firstNode).closest('.content-scrollable');
+        var parentTop = $parent.offset().top;
+        $parent.scrollTop(top - parentTop);
+    }
+});
+
+Template.training_item.helpers({
+    firstName: function() {
+        return this.userName.split(' ')[0];
+    },
+    recipeTitle: function() {
+        return RecipesData[this.trainingName].title;
+    },
+    path: function() {
+        return Router.path('recipe', { name: this.trainingName },
+            { query: { trainingId: this._id } })
+    }
+})

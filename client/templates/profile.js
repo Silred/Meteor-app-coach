376e7badf3fedc72965f8b8383469a984e95c8d2
momolafripouille <Nicolas-g7@hotmail.fr>
2016-06-12 19:15:14 +0200

    page profile affiche les recettes et les trainings de la personne

--- client/templates/profile.js
+++ client/templates/profile.js
@@ -4,6 +4,12 @@ Template.profile.helpers({
     profiles: function() {
         return Meteor.users.find({ _id: Router.current().params._id});
     },
+    trainings: function() {
+        return Trainings.find({ id: Router.current().params._id});
+    },
+    recipes: function() {
+        return Recipes.find({ id: Router.current().params._id});
+    },
     ready: function() {
         return Router.current().profile.ready();
 
